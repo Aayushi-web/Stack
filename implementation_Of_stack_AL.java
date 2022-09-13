51ddfcb94d32c7e505c0f89fceb27e9a0441a482
Aayushi Mishra <mishraaayushi66@gmail.com>
2022-09-14 01:30:59 +0530

    implementataion of stck using ArrayList

--- implementation_Of_stack_AL.java
+++ implementation_Of_stack_AL.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class implementation_Of_stack_AL{
+     static class stck{
+      static ArrayList<Integer> list=new ArrayList<>();
+        public static boolean isEmpty(){
+return (list.size()==0);
+        }
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+        //pop
+        public static int pop(){
+            if (isEmpty()){
+                return -1;
+            }
+          int top=list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+
+        }
+        //peek
+        public static int  peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top=list.get(list.size()-1);
+            return top;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println();
+       stck stack= new stck();
+       stack.push(1);
+       stack.push(2);
+       stack.push(5);
+       stack.push(4);
+       while(!stack.isEmpty()){
+        System.out.print(stack.peek()+" ");
+        stack.pop();
+
+       }
     }
 }
